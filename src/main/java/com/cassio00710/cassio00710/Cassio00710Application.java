7262e4781954bdfe5ca4eab7c7796643bc8cc6d0
CassioMoreira <cassio00710@hotmail.com>
2021-12-27 20:27:30 -0300

    first commit:structure created

---
+++ src/main/java/com/cassio00710/cassio00710/Cassio00710Application.java
@@ -0,0 +1,13 @@
+package com.cassio00710.cassio00710;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cassio00710Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cassio00710Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/cassio00710/cassio00710/Cassio00710ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cassio00710.cassio00710;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cassio00710ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
